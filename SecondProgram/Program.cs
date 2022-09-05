a93fe5893ac31e8a39c711a74a422969f4dd60d8
Eric <ericismag@outlook.com>
2022-09-05 23:45:20 +0300

    Second Program

---
+++ SecondProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine ("Введите первое число");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите третье число");
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if (FirstNumber > SecondNumber)
+{
+    if (FirstNumber > SecondNumber)
+        Console.WriteLine ("Max = " + FirstNumber);
+}
+else
+{
+    if (SecondNumber > ThirdNumber)
+        Console.WriteLine ("Max = " + SecondNumber);
+    else
+        Console.WriteLine ("Max = " + ThirdNumber);
+}
