0401318a55a9e9ca975fe7e9a58e08a2c071fc5d
Eric <ericismag@outlook.com>
2022-09-05 23:50:34 +0300

    Third Program

---
+++ ThirdProgram/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0)
+    Console.WriteLine("Число четное");
+else
+    Console.WriteLine ("Число нечетное");
