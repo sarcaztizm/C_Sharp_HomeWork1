048c40558083f361892a96ac703d0c011f3011ce
Eric <ericismag@outlook.com>
2022-09-06 00:00:44 +0300

    FourthProgram

---
+++ FourthProgram/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= N)
+{
+    if (count %2 == 0) 
+        Console.Write ("    " + count);
+    count++;
+}
